a3272a170d2657a57713259e0de6a212620b7617
Alexey Eremikhin <alexxz@alexxz.ru>
2012-07-03 23:21:03 +0400

    Bugfix bugfix allowing users wrong moves

--- src/main.erl
+++ src/main.erl
@@ -36,18 +36,25 @@ client_loop(Socket, MovesX, MovesY) ->
 					client_loop(Socket, [], []);
 				
 				{"/play",[{"x",Xin}, {"y",Yin}, {"aggress", Ain}]} ->
-					X = list_to_integer(Xin), Y = list_to_integer(Yin), 
-					A = case string:to_float(Ain) of {F, []} -> F; {error, no_float} -> list_to_integer(Ain) end,
-					Aggress = case  A < 0.1 orelse A > 3.6 of true -> 0.8; false -> A end, % проверка на корректность
-					io:format("Aggress ~p~n", [Aggress]),
-					TmpX = MovesX ++ [{X, Y}],
-					{_,{X2,Y2}} = computer_logic:get_bot_move(TmpX, MovesY, Aggress),
-					TmpY = MovesY ++ [{X2,Y2}],
-					Res = case check_winner:cw(TmpX, TmpY, {X,Y}, {X2, Y2}, []) of
-						bot_win -> [{lose,1},{x,X2},{y,Y2}];
-						player_win -> [{win,1}];
-						draw -> [{draw,1}];
-						next -> [{x,X2}, {y,Y2}]
+					X = list_to_integer(Xin), Y = list_to_integer(Yin),
+					Res = case internal:exist_in_list(lists:concat([MovesX, MovesY]), {X, Y}) of
+					true  ->
+						TmpX = MovesX,
+						TmpY = MovesY,
+						[{wrong, 1}];
+					false ->
+						A = case string:to_float(Ain) of {F, []} -> F; {error, no_float} -> list_to_integer(Ain) end,
+						Aggress = case  A < 0.1 orelse A > 3.6 of true -> 0.8; false -> A end, % проверка на корректность
+						io:format("Aggress ~p~n", [Aggress]),
+						TmpX = MovesX ++ [{X, Y}],
+						{_,{X2,Y2}} = computer_logic:get_bot_move(TmpX, MovesY, Aggress),
+						TmpY = MovesY ++ [{X2,Y2}],
+						case check_winner:cw(TmpX, TmpY, {X,Y}, {X2, Y2}, []) of
+							bot_win -> [{lose,1},{x,X2},{y,Y2}];
+							player_win -> [{win,1}];
+							draw -> [{draw,1}];
+							next -> [{x,X2}, {y,Y2}]
+						end
 					end,
 					D = simple_json_encode(Res),
 					ok = gen_tcp:send(Socket, internal:response(200, D)),
