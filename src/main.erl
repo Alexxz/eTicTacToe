685ee2701c4b0f31544af3ec8a34c01b8a798b61
Alexey Eremikhin <alexxz@alexxz.ru>
2012-07-16 18:59:45 +0400

    Add logging for debug

--- src/main.erl
+++ src/main.erl
@@ -53,24 +53,30 @@ client_loop(Socket, MovesX, MovesY) ->
 				
 				{"/play",[{"x",Xin}, {"y",Yin}, {"aggress", Ain}]} ->
 					X = list_to_integer(Xin), Y = list_to_integer(Yin),
+					A = case string:to_float(Ain) of {F, []} -> F; {error, no_float} -> list_to_integer(Ain) end,
+					Aggress = case  A < 0.1 orelse A > 3.6 of true -> 0.8; false -> A end, % проверка на корректность
+					log("Player: ~p aggr ~p", [{X,Y}, Aggress]),
+
 					Res = case internal:exist_in_list(lists:concat([MovesX, MovesY]), {X, Y}) of
 					true  ->
 						TmpX = MovesX,
 						TmpY = MovesY,
+						log("Result: wrong move", []),
 						[{wrong, 1}];
 					false ->
-						A = case string:to_float(Ain) of {F, []} -> F; {error, no_float} -> list_to_integer(Ain) end,
-						Aggress = case  A < 0.1 orelse A > 3.6 of true -> 0.8; false -> A end, % проверка на корректность
 						io:format("Aggress ~p~n", [Aggress]),
 						TmpX = MovesX ++ [{X, Y}],
 						{_,{X2,Y2}} = computer_logic:get_bot_move(TmpX, MovesY, Aggress),
 						TmpY = MovesY ++ [{X2,Y2}],
-						case check_winner:cw(TmpX, TmpY, {X,Y}, {X2, Y2}, []) of
+						log("Bot: ~p", [{X2,Y2}]),
+						Response = case check_winner:cw(TmpX, TmpY, {X,Y}, {X2, Y2}, []) of
 							bot_win -> [{lose,1},{x,X2},{y,Y2}];
 							player_win -> [{win,1}];
 							draw -> [{draw,1}, {x,X2}, {y,Y2}];
 							next -> [{x,X2}, {y,Y2}]
-						end
+						end,
+						log("Response: ~p", [Response]),
+						Response
 					end,
 					D = simple_json_encode(Res),
 					ok = gen_tcp:send(Socket, internal:response(200, D)),
@@ -107,4 +113,13 @@ simple_json_encode_el([Pair|Tail]) ->
 simple_json_pack_pair({Name, Val}) ->
 	["\"", atom_to_list(Name), "\":", integer_to_list(Val)].
 
+
+
+log(Message)->
+	log(Message, []).
+
+log(Message, Args) ->
+	{ok, File} = file:open("log", [write, append]),
+	ok = io:format(File, "~p: " ++ Message ++ "~n", [calendar:now_to_datetime(now())| Args]),
+	ok = file:close(File).
 
